5476d5c1be36b803d6df6caa025691305c547c25
Rohit Bhumar <61305057+rohithbhumar@users.noreply.github.com>
2023-06-21 10:53:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'database.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'users.db')
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 
